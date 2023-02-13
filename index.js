6f8c5d323dc9f16a36d652d5fd851c264c145947
Carlo <125126731+hoopoeBird@users.noreply.github.com>
2023-02-13 18:07:42 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/indexC.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caesar+Dressing&family=Dongle&family=Metal+Mania&family=Open+Sans:wght@300&family=Ubuntu&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Do Not Edit HTML -->
+    <div></div>
+    <script src="main.js"></script>
+</body>
+</html>
